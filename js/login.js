26c9f5cccfa0758a662c65cf50cf6f0a487cc809
antony-dennis <dennis27rosa@gmail.com>
2021-08-20 08:22:01 -0300

    proyecto-japV1.0

--- js/login.js
+++ js/login.js
@@ -1,6 +1,44 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function(e){
+
+    document.getElementById("boton-login").addEventListener("click",function(){
+        let inputEmail = document.getElementById("inputEmail");
+        let inputPassword = document.getElementById("inputPassword");
+
+        if(inputEmail.value ===''){
+
+            alert("El campo Email esta vacio , Completelo para continuar");
+        }
+        else { 
+        
+         
+            if(inputPassword.value ===''){
+                
+                alert("El campo Password esta vacio , Completelo para continuar");
+            }
+            else { 
+                
+                    window.location = 'index.html';
+                    
+                
+            
+            }
+            
+        
+        
+        }
+       
+        
+        
+    
+    })
 
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,66 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+var productosArray=[];
+
+
+
+
+
+
+
+function showCategoriesList(){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < productosArray.length; i++){
+        let producto = productosArray[i];
+
+        
+
+            htmlContentToAppend += `
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + producto.imgSrc + `" alt="` + producto.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ producto.name +`</h4>
+                            <small class="text-muted">` + producto.currency +` `+ producto.cost+ `</small>
+                        </div>
+                        <p class="mb-1">` + producto.description + `</p>
+                    </div>
+                </div>
+            </a>
+            `
+        
+
+        document.getElementById("productos-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function(resultado){
+        if(resultado.status === "ok"){
+            productosArray=resultado.data;
+            //llamar funcion con la que hago cosas
+            showCategoriesList();
+            
+        }
+    });
 
 });
